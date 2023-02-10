32a42bd04c77e29dab02d52df0e0e76f4958796b
Tim Miller <miller.tim108@gmail.com>
2023-02-10 09:23:56 -0500

    functions to get a tuple of county totals and sort them

--- assignment-two/assignment_two.py
+++ assignment-two/assignment_two.py
@@ -1,10 +1,13 @@
-# Data Source https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_5m.json
 # Read from a local file
 import json
 import io
 
+"""Task 1
+Open geojson file
+"""
 # Use "with ... as ..." to better handle exceptions
 # https://stackoverflow.com/questions/30996289/utf8-codec-cant-decode-byte-0xf3
+# Data Source https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_5m.json
 with io.open('assignment-two/data/gz_2010_us_050_00_5m.json', encoding='latin-1') as f:
   data = json.load(f)
 
@@ -28,13 +31,52 @@ for feature in features:
   state_code = properties['STATE']
   county_states.setdefault(county_name, []).append(state_code)
 
-top_n = 3
-top_counties = [None] * top_n
-
 print(first_feature['properties'])
 print(county_states['Wade Hampton'])
 
-max_county = ''
-max_county_total = 0
-for county_state in county_states:
-  county_state
+def get_county_totals(county_states):
+  """Reformat the counties and state list objects into a tuple of counties and their totals
+
+  Arguments:
+  county_states dict[str, list<str>]: County name as the key and the list of state codes as the value
+
+  Returns:
+  tuple[str, int]: The counties and the total number of states that use them.
+  """
+  county_totals = [(None, None)] * len(county_states) 
+  i = 0
+  for county, states in county_states:
+    county_total = (county, len(states))
+    county_totals[i] = county_total
+    i+=1
+
+  return county_totals
+
+
+def test_get_county_totals():
+  pass
+
+
+def top_k_sort_k(totals, k):
+  """Find the top k values in a tuple of objects and their counts.
+  
+  Arguments:
+  totals -- A tuple with a name of type string in the first position and a total of type integer in the second position
+  k -- the number of items to rank
+
+  Returns:
+  a tuple with the top k items and their counts
+  
+  Note:
+  This function iterates through the list of items, only sorting the list of rankings
+  """
+  top_k = [0]*k
+  for total in totals:
+    total_val = total[1]
+    bottom_k_val = top_k[0][1]
+    if total_val > bottom_k_val:
+      top_k[0] = total
+      top_k.sort(key=lambda a: a[1])
+
+def test_top_k_sort_k():
+  pass
