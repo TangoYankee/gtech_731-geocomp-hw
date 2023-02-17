a52ef773917ba822467a1ab71222a64161c217f2
tangoyankee <miller.tim108@gmail.com>
2023-02-16 19:41:39 -0500

    custom sort function, not recursive

--- assignment-three/assignment_three.py
+++ assignment-three/assignment_three.py
@@ -134,6 +134,69 @@ def test_get_county_totals():
 
 test_get_county_totals()
 
+
+def recursive_bubble(totals):
+  """Sorting based on the assumption that only the last item needs to be 'bubbled up' to its correct space
+
+  Arguments:
+  totals list[tuple[str, int]] -- Item Name and total 
+  
+  Returns:
+  None: list is mutated in place 
+  """
+
+  target_pos = 0
+  compare_pos = 1
+  totals_count = len(totals)
+  while compare_pos < totals_count and totals[target_pos][1] > totals[compare_pos][1]:
+    totals[target_pos], totals[compare_pos] = totals[compare_pos], totals[target_pos]
+    target_pos += 1
+    compare_pos += 1
+
+def test_recursive_bubble():
+    mock_totals = [("C", 5), ("B", 0), ("A", 5)]
+    expected_totals = [("B", 0), ("C", 5), ("A", 5)]
+
+    # Sort in place
+    recursive_bubble(mock_totals) 
+
+    assert(mock_totals == expected_totals)
+
+test_recursive_bubble() 
+
+def top_k_sort_k_recursive(totals, k):
+  """Find the top k values in a tuple of objects and their counts.
+  
+  Arguments:
+  totals tuple[str, int]-- Item Name and total 
+  k int -- the number of items to rank
+
+  Returns:
+  tuple[str, int] -- Top k items and their counts
+  
+  Note:
+  This function iterates through the list of items, only sorting the list of rankings
+  """
+  top_k = [(None, 0)]*k
+  for total in totals:
+    total_val = total[1]
+    bottom_k_val = top_k[0][1]
+    if total_val > bottom_k_val:
+      top_k[0] = total
+      recursive_bubble(top_k)
+
+  return top_k
+
+def test_top_k_sort_k_recursive():
+  mock_county_totals = [("A", 0), ("B", 1), ("C", 2), ("D", 3), ("E", 4), ("F", 4)]
+  expected_top_k = [("D", 3), ("F", 4), ("E", 4)]
+
+  obs = top_k_sort_k_recursive(mock_county_totals, 3)
+
+  assert(obs == expected_top_k)
+
+test_top_k_sort_k_recursive()
+
 def top_k_sort_k(totals, k):
   """Find the top k values in a tuple of objects and their counts.
   
@@ -190,12 +253,12 @@ test_top_k_sort_all()
 
 county_totals = get_county_totals(county_states)
 """
-Compare the two algorithims to determine which has better performance.
+Compare the built-in and recursive sort algorithm approaches
 
 Findings: sorting the rankings list is generally faster when k is less than 25.
 Sorting the whole list of counties and taking the top is generally faster when k is more than 25.
 """
-k = 3 
+k = 200 
 start_time = time.perf_counter()
 top_counties = top_k_sort_k(county_totals, k)
 stop_time = time.perf_counter()
@@ -203,291 +266,291 @@ print(f"top counties: {top_counties}")
 print(f"top counter time: {stop_time - start_time}")
 
 start_time = time.perf_counter()
-top_counties_alt = top_k_sort_all(county_totals, k)
+top_counties_rec = top_k_sort_k_recursive(county_totals, k)
 stop_time = time.perf_counter()
-print(f"top counties alt: {top_counties_alt}")
+print(f"top counties alt: {top_counties_rec}")
 print(f"top counter alt time: {stop_time - start_time}")
 
 
-"""
-Display the top counties and their states
-"""
-for top_county in top_counties:
-  name = top_county[0]
-  print(f"{name} county appears in state codes: {county_states[name]}")
+# """
+# Display the top counties and their states
+# """
+# for top_county in top_counties:
+#   name = top_county[0]
+#   print(f"{name} county appears in state codes: {county_states[name]}")
 
 
-"""Task 3
+# """Task 3
 
-Basic statistics by state
-"""
+# Basic statistics by state
+# """
 
-"""Task 3, part one
+# """Task 3, part one
 
-The number of counties in each state
-"""
-def get_state_counties_total(features):
-  """Find the total number of counties in each state
+# The number of counties in each state
+# """
+# def get_state_counties_total(features):
+#   """Find the total number of counties in each state
 
-  Arguments:
-  features list[dict] -- each county has a set of properties, state code is most relevant
+#   Arguments:
+#   features list[dict] -- each county has a set of properties, state code is most relevant
 
-  Returns:
-  dict -- key is state code and value is the total number of counties 
-  """
-  totals = {}
-  for feature in features:
-    properties = feature['properties']
-    state_code = properties['STATE']
-    totals[state_code] = totals.get(state_code, 0) + 1
+#   Returns:
+#   dict -- key is state code and value is the total number of counties 
+#   """
+#   totals = {}
+#   for feature in features:
+#     properties = feature['properties']
+#     state_code = properties['STATE']
+#     totals[state_code] = totals.get(state_code, 0) + 1
 
-  return totals
-
-
-def test_get_state_counties_total():
-  mock_features = [{
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US01087',
-      'STATE': '01',
-      'COUNTY': '087',
-      'NAME': 'Macon',
-      'LSAD': 'County',
-      'CENSUSAREA': 608.885
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US02275',
-      'STATE': '02',
-      'COUNTY': '275',
-      'NAME': 'Wrangell',
-      'LSAD': 'Cty&Bor',
-      'CENSUSAREA': 2541.483
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-      'properties': {
-      'GEO_ID': '0500000US02270',
-      'STATE': '02',
-      'COUNTY': '270',
-      'NAME': 'Wade Hampton',
-      'LSAD': 'CA',
-      'CENSUSAREA': 17081.433
-    },
-    "geometry": None,
-  }] 
-
-  expected_totals = {
-    '01': 1,
-    '02': 2
-  }
-
-  assert(get_state_counties_total(mock_features) == expected_totals)
-
-test_get_state_counties_total()
-state_counties_totals = get_state_counties_total(features)
-print(f"List of {len(state_counties_totals)} states' county totals: {state_counties_totals}")
+#   return totals
 
 
-"""Task 3, part two
+# def test_get_state_counties_total():
+#   mock_features = [{
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US01087',
+#       'STATE': '01',
+#       'COUNTY': '087',
+#       'NAME': 'Macon',
+#       'LSAD': 'County',
+#       'CENSUSAREA': 608.885
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US02275',
+#       'STATE': '02',
+#       'COUNTY': '275',
+#       'NAME': 'Wrangell',
+#       'LSAD': 'Cty&Bor',
+#       'CENSUSAREA': 2541.483
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#       'properties': {
+#       'GEO_ID': '0500000US02270',
+#       'STATE': '02',
+#       'COUNTY': '270',
+#       'NAME': 'Wade Hampton',
+#       'LSAD': 'CA',
+#       'CENSUSAREA': 17081.433
+#     },
+#     "geometry": None,
+#   }] 
 
-Name and size of the biggest and smallest county in each state, by area
-"""
-def get_state_county_min_max_area(features):
-  """Find the name and size of the biggest and smallest county in each state
+#   expected_totals = {
+#     '01': 1,
+#     '02': 2
+#   }
 
-  Arguments:
-  features list[dict] -- county object with type, properties, and geometry
+#   assert(get_state_counties_total(mock_features) == expected_totals)
 
-  Returns:
-  dict[str, dict] -- county data for largest and smallest counties
-  """
-  state_county_min_max_area = {}
+# test_get_state_counties_total()
+# state_counties_totals = get_state_counties_total(features)
+# print(f"List of {len(state_counties_totals)} states' county totals: {state_counties_totals}")
 
-  for feature in features:
-    properties = feature['properties']
-    state_code = properties['STATE']
-    county_name = properties['NAME']
-    county_area = properties['CENSUSAREA']
 
-    county = {
-      "name": county_name,
-      "area": county_area
-    }
+# """Task 3, part two
 
-    if state_code in state_county_min_max_area:
-      state = state_county_min_max_area[state_code]
+# Name and size of the biggest and smallest county in each state, by area
+# """
+# def get_state_county_min_max_area(features):
+#   """Find the name and size of the biggest and smallest county in each state
 
-      largest_county_area = state['largest_county']['area']
-      if county_area > largest_county_area:
-        state['largest_county'] = county
+#   Arguments:
+#   features list[dict] -- county object with type, properties, and geometry
 
-      smallest_county_area = state['smallest_county']['area']
-      if county_area < smallest_county_area:
-        state['smallest_county'] = county
-    else:
-      state = {
-        "largest_county": county,
-        "smallest_county": county
-      }
+#   Returns:
+#   dict[str, dict] -- county data for largest and smallest counties
+#   """
+#   state_county_min_max_area = {}
 
-      state_county_min_max_area[state_code] = state
+#   for feature in features:
+#     properties = feature['properties']
+#     state_code = properties['STATE']
+#     county_name = properties['NAME']
+#     county_area = properties['CENSUSAREA']
 
-  return state_county_min_max_area
+#     county = {
+#       "name": county_name,
+#       "area": county_area
+#     }
 
-def test_get_state_county_min_max_area():
-  mock_features = [{
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US01087',
-      'STATE': '01',
-      'COUNTY': '087',
-      'NAME': 'Macon',
-      'LSAD': 'County',
-      'CENSUSAREA': 608.885
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US02275',
-      'STATE': '02',
-      'COUNTY': '275',
-      'NAME': 'Wrangell',
-      'LSAD': 'Cty&Bor',
-      'CENSUSAREA': 2541.483
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-      'properties': {
-      'GEO_ID': '0500000US02270',
-      'STATE': '02',
-      'COUNTY': '270',
-      'NAME': 'Wade Hampton',
-      'LSAD': 'CA',
-      'CENSUSAREA': 17081.433
-    },
-    "geometry": None,
-  }]
+#     if state_code in state_county_min_max_area:
+#       state = state_county_min_max_area[state_code]
 
-  expected = {
-    '01': {
-      'largest_county': {
-        'name': "Macon",
-        "area": 608.885
-      },
-      "smallest_county": {
-        'name': "Macon",
-        "area": 608.885
-      }
-    },
-    '02': {
-      "largest_county": {
-        "name": "Wade Hampton",
-        "area": 17081.433
-      },
-      "smallest_county": {
-        "name": "Wrangell",
-        "area": 2541.483
-      }
-    }
-  }
+#       largest_county_area = state['largest_county']['area']
+#       if county_area > largest_county_area:
+#         state['largest_county'] = county
 
-  assert(get_state_county_min_max_area(mock_features) == expected)
+#       smallest_county_area = state['smallest_county']['area']
+#       if county_area < smallest_county_area:
+#         state['smallest_county'] = county
+#     else:
+#       state = {
+#         "largest_county": county,
+#         "smallest_county": county
+#       }
 
-test_get_state_county_min_max_area()
-state_county_min_max_area = get_state_county_min_max_area(features)
-print(f"min and max counties by area in each state: {state_county_min_max_area}")
+#       state_county_min_max_area[state_code] = state
 
-"""Task 3, part three
+#   return state_county_min_max_area
 
-The total and average area of counties in each state
-"""
-def get_state_total_avg_area_county(features):
-  """ The total and average area of counties in each state
+# def test_get_state_county_min_max_area():
+#   mock_features = [{
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US01087',
+#       'STATE': '01',
+#       'COUNTY': '087',
+#       'NAME': 'Macon',
+#       'LSAD': 'County',
+#       'CENSUSAREA': 608.885
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US02275',
+#       'STATE': '02',
+#       'COUNTY': '275',
+#       'NAME': 'Wrangell',
+#       'LSAD': 'Cty&Bor',
+#       'CENSUSAREA': 2541.483
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#       'properties': {
+#       'GEO_ID': '0500000US02270',
+#       'STATE': '02',
+#       'COUNTY': '270',
+#       'NAME': 'Wade Hampton',
+#       'LSAD': 'CA',
+#       'CENSUSAREA': 17081.433
+#     },
+#     "geometry": None,
+#   }]
 
-  Arguments:
-  features list[dict] -- county data
+#   expected = {
+#     '01': {
+#       'largest_county': {
+#         'name': "Macon",
+#         "area": 608.885
+#       },
+#       "smallest_county": {
+#         'name': "Macon",
+#         "area": 608.885
+#       }
+#     },
+#     '02': {
+#       "largest_county": {
+#         "name": "Wade Hampton",
+#         "area": 17081.433
+#       },
+#       "smallest_county": {
+#         "name": "Wrangell",
+#         "area": 2541.483
+#       }
+#     }
+#   }
 
-  Returns:
-  dict[str, dict] -- total and avg area for counties in the state
-  """
-  state_total_avg_area_county = {}
-  for feature in features:
-    properties = feature["properties"]
-    state_code = properties['STATE']
-    county_area = properties['CENSUSAREA']
+#   assert(get_state_county_min_max_area(mock_features) == expected)
 
-    if state_code in state_total_avg_area_county:
-      state = state_total_avg_area_county[state_code]
-      state['county_total_area'] = state['county_total_area'] + county_area
-      state['county_count'] = state['county_count'] + 1
-      state['county_avg_area'] = state['county_total_area'] / state['county_count']
-    else:
-      state = {
-        "county_total_area": county_area,
-        "county_avg_area": county_area,
-        "county_count": 1
-      }
-      state_total_avg_area_county[state_code] = state
+# test_get_state_county_min_max_area()
+# state_county_min_max_area = get_state_county_min_max_area(features)
+# print(f"min and max counties by area in each state: {state_county_min_max_area}")
 
-  return state_total_avg_area_county
+# """Task 3, part three
 
-def test_get_state_total_avg_area_county():
-  mock_features = [{
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US01087',
-      'STATE': '01',
-      'COUNTY': '087',
-      'NAME': 'Macon',
-      'LSAD': 'County',
-      'CENSUSAREA': 608.885
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-    "properties": {
-      'GEO_ID': '0500000US02275',
-      'STATE': '02',
-      'COUNTY': '275',
-      'NAME': 'Wrangell',
-      'LSAD': 'Cty&Bor',
-      'CENSUSAREA': 2541.483
-    },
-    "geometry": None,
-  }, {
-    'type': 'Feature',
-      'properties': {
-      'GEO_ID': '0500000US02270',
-      'STATE': '02',
-      'COUNTY': '270',
-      'NAME': 'Wade Hampton',
-      'LSAD': 'CA',
-      'CENSUSAREA': 17081.433
-    },
-    "geometry": None,
-  }]
+# The total and average area of counties in each state
+# """
+# def get_state_total_avg_area_county(features):
+#   """ The total and average area of counties in each state
 
-  expected = {
-    '01': {
-      "county_total_area": 608.885,
-      "county_avg_area": 608.885,
-      "county_count": 1
-    }, 
-    '02': {
-      "county_total_area": 19622.916,
-      "county_avg_area": 9811.458,
-      "county_count": 2
-    }
-  }
+#   Arguments:
+#   features list[dict] -- county data
 
-  assert(get_state_total_avg_area_county(mock_features) == expected)
+#   Returns:
+#   dict[str, dict] -- total and avg area for counties in the state
+#   """
+#   state_total_avg_area_county = {}
+#   for feature in features:
+#     properties = feature["properties"]
+#     state_code = properties['STATE']
+#     county_area = properties['CENSUSAREA']
 
-test_get_state_total_avg_area_county()
-state_total_avg_area_county = get_state_total_avg_area_county(features)
-print(f"County areas for states: {state_total_avg_area_county}")
+#     if state_code in state_total_avg_area_county:
+#       state = state_total_avg_area_county[state_code]
+#       state['county_total_area'] = state['county_total_area'] + county_area
+#       state['county_count'] = state['county_count'] + 1
+#       state['county_avg_area'] = state['county_total_area'] / state['county_count']
+#     else:
+#       state = {
+#         "county_total_area": county_area,
+#         "county_avg_area": county_area,
+#         "county_count": 1
+#       }
+#       state_total_avg_area_county[state_code] = state
+
+#   return state_total_avg_area_county
+
+# def test_get_state_total_avg_area_county():
+#   mock_features = [{
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US01087',
+#       'STATE': '01',
+#       'COUNTY': '087',
+#       'NAME': 'Macon',
+#       'LSAD': 'County',
+#       'CENSUSAREA': 608.885
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#     "properties": {
+#       'GEO_ID': '0500000US02275',
+#       'STATE': '02',
+#       'COUNTY': '275',
+#       'NAME': 'Wrangell',
+#       'LSAD': 'Cty&Bor',
+#       'CENSUSAREA': 2541.483
+#     },
+#     "geometry": None,
+#   }, {
+#     'type': 'Feature',
+#       'properties': {
+#       'GEO_ID': '0500000US02270',
+#       'STATE': '02',
+#       'COUNTY': '270',
+#       'NAME': 'Wade Hampton',
+#       'LSAD': 'CA',
+#       'CENSUSAREA': 17081.433
+#     },
+#     "geometry": None,
+#   }]
+
+#   expected = {
+#     '01': {
+#       "county_total_area": 608.885,
+#       "county_avg_area": 608.885,
+#       "county_count": 1
+#     }, 
+#     '02': {
+#       "county_total_area": 19622.916,
+#       "county_avg_area": 9811.458,
+#       "county_count": 2
+#     }
+#   }
+
+#   assert(get_state_total_avg_area_county(mock_features) == expected)
+
+# test_get_state_total_avg_area_county()
+# state_total_avg_area_county = get_state_total_avg_area_county(features)
+# print(f"County areas for states: {state_total_avg_area_county}")
